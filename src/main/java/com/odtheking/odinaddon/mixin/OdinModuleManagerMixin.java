d5dbbb19031caf6ba9af224304e76c59014656b3
poisonous888 <rwlittle888@gmail.com>
2026-03-04 15:42:52 -0500

    0.0.2 improved dev module handling (each gets its own file so they dont reset)

--- src/main/java/com/odtheking/odinaddon/mixin/OdinModuleManagerMixin.java
+++ src/main/java/com/odtheking/odinaddon/mixin/OdinModuleManagerMixin.java
@@ -1,11 +1,16 @@
 package com.odtheking.odinaddon.mixin;
-
+import com.odtheking.odin.config.ModuleConfig;
 import com.odtheking.odin.features.Module;
 import com.odtheking.odin.features.ModuleManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/*
 @Mixin({ModuleManager.class})
 public abstract class OdinModuleManagerMixin {
     @Redirect(
@@ -14,6 +19,32 @@ public abstract class OdinModuleManagerMixin {
                     value = "INVOKE",
                     target = "Lcom/odtheking/odin/features/Module;isDevModule()Z"
             )
+    )
+    private boolean noDevModules(Module no) {
+        return false;
+    }
+}
+*/
+
+@Mixin(value = ModuleManager.class, remap = false)
+public class OdinModuleManagerMixin{
+    @ModifyVariable(method = "registerModules", at = @At("HEAD"), argsOnly = true, ordinal = 0)
+    private static Module[] separateDevModules(Module[] modules, ModuleConfig config) {
+        if(modules.length<2){return modules;}
+        Stream<Module> dev=Arrays.stream(modules).filter(Module::isDevModule);
+        Stream<Module> out=Arrays.stream(modules).filter(module -> !module.isDevModule());
+        ModuleManager modman=ModuleManager.INSTANCE;
+        for(Module reregester:dev.toArray(Module[]::new)){
+            modman.registerModules(new ModuleConfig(reregester.getName()+".json"),reregester);
+        }
+        return out.toArray(Module[]::new);
+    }
+    @Redirect(
+        method = {"registerModules"},
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/odtheking/odin/features/Module;isDevModule()Z"
+        )
     )
     private boolean noDevModules(Module no) {
         return false;
