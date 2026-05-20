7728308b6848b9814a49dd2846fe9130a21f9cda
poisonous888 <rwlittle888@gmail.com>
2026-05-20 13:07:10 -0400

    changed how skulls and blocks are hidden

--- src/main/java/psn/zirconium/mixin/ArmorSkullMixin.java
+++ src/main/java/psn/zirconium/mixin/ArmorSkullMixin.java
@@ -1,22 +1,27 @@
 package psn.zirconium.mixin;
 
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.decoration.ArmorStand;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import psn.zirconium.features.HideArmor;
 
 @Mixin(LivingEntityRenderer.class)
-public class ArmorSkullMixin{
-    @Redirect(
+public class ArmorSkullMixin<T extends LivingEntity, S extends LivingEntityRenderState>{
+    @Inject(
         method="extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V",
-        at=@At(value="INVOKE", target="Lnet/minecraft/world/item/BlockItem;getBlock()Lnet/minecraft/world/level/block/Block;")
-    )
-    private Block hideSkull(BlockItem instance){
-        if(HideArmor.checkSkull()) return Blocks.AIR;
-        return instance.getBlock();
+        at=@At("TAIL"))
+    private void noSkull(T livingEntity, S livingEntityRenderState, float f, CallbackInfo ci){
+        if(HideArmor.checkSkull()){
+            if(livingEntity instanceof ArmorStand){
+                return;
+            }
+            livingEntityRenderState.headItem.clear();
+            livingEntityRenderState.wornHeadType=null;
+        }
     }
 }
