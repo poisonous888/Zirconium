dac838edf5b28a53ddbd08f1f01a739048558d34
poisonous888 <rwlittle888@gmail.com>
2026-06-20 19:25:24 -0400

    fixed anvil helper and improved GuiHighlight processing

--- src/main/kotlin/psn/zirconium/features/GuiHighlight.kt
+++ src/main/kotlin/psn/zirconium/features/GuiHighlight.kt
@@ -4,14 +4,13 @@ import com.odtheking.odin.clickgui.settings.Setting.Companion.withDependency
 import com.odtheking.odin.clickgui.settings.impl.BooleanSetting
 import com.odtheking.odin.clickgui.settings.impl.ColorSetting
 import com.odtheking.odin.events.GuiEvent
+import com.odtheking.odin.events.ScreenEvent
+import com.odtheking.odin.events.core.EventBus
 import com.odtheking.odin.events.core.on
-import com.odtheking.odin.events.core.onReceive
 import com.odtheking.odin.features.Module
 import com.odtheking.odin.utils.Colors
-import com.odtheking.odin.utils.alert
 import com.odtheking.odin.utils.customData
 import com.odtheking.odin.utils.loreString
-import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import psn.zirconium.ZirconiumEntry
@@ -26,67 +25,101 @@ object GuiHighlight : Module(
     private val highlightCommission by BooleanSetting("Highlight Completed Commissions", false, desc = "displays a colored background behind completed commissions")
     private val commissionColor by ColorSetting("Commission Color",Colors.MINECRAFT_GREEN,false,"the highlight color").withDependency { highlightCommission }
     private val highlightAnvil by BooleanSetting("Highlight Like Books In Anvil", false, desc = "skytils anvil helper")
-    //private val blockWrongAnvil by BooleanSetting("Block Incompatible Books", false, desc = "prevents you from combining books with different enchantment levels").withDependency { highlightAnvil }
+    private val blockWrongAnvil by BooleanSetting("Block Incompatible Books", false, desc = "prevents you from combining books with different enchantment levels").withDependency { highlightAnvil }
     private val anvilColor by ColorSetting("Anvil Highlight Color",Colors.MINECRAFT_AQUA,false,"the highlight color").withDependency { highlightAnvil }
     private val petRegex = Regex("Pets(?: \\((\\d)/(\\d)\\))?")
-    private var switch = 0
-    private var curBook=""
-    private var curLvl=0
-    init {
-        onReceive<ClientboundOpenScreenPacket>{
-            val title=title.string
-            if(petRegex.containsMatchIn(title)){
-                switch=1
-                return@onReceive
+    
+    //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
+    
+    private var curHelper:Any?=null
+    init{
+        on<ScreenEvent.Open>{
+            if(highlightCommission&&screen.title.string=="Commissions"){
+                EventBus.subscribe(CommHelper)
+                curHelper=CommHelper
             }
-            if(title == "Commissions"){
-                switch=2
-                return@onReceive
+            if(highlightAnvil&&screen.title.string=="Anvil"){
+                EventBus.subscribe(BookHelper)
+                curHelper=BookHelper
             }
-            if(title == "Anvil"){
-                switch=3
-                return@onReceive
+            if(highlightPet&&petRegex.containsMatchIn(screen.title.string)){
+                EventBus.subscribe(PetHelper)
+                curHelper=PetHelper
             }
-            switch=0
         }
-        on<GuiEvent.RenderSlot>{
-            if(switch==0)return@on
-            val lore=slot.item.loreString
-            if(switch==1){
-                if(highlightPet&&lore.contains("Click to despawn!")){
-                    guiGraphics.fill(slot.x, slot.y, slot.x + 16, slot.y + 16, petColor.rgba)
-                    guiGraphics.renderFakeItem(slot.item,slot.x, slot.y)
+        on<ScreenEvent.Close>{
+            EventBus.unsubscribe(curHelper?:return@on)
+            curHelper=null
+        }
+    }
+    
+    //--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//
+    
+    private object BookHelper{
+        private var blocked = false
+        private var book = ""
+        private var lvl = -1
+        init {
+            on<GuiEvent.RenderSlot>{
+                if(!slot.item.customData.contains("enchantments")) return@on
+                val enchant=slot.item.customData.get("enchantments")?.asCompound()?.get()?.entrySet()?.first()
+                val bookCur=enchant?.key.toString()
+                val lvlCur=enchant?.value.toString().toInt()
+                if(lvlCur==lvl&&bookCur==book){
+                    guiGraphics.fill(slot.x, slot.y, slot.x + 16, slot.y + 16, anvilColor.rgba)
                 }
-                return@on
+                if(slot.index==29){
+                    if(slot.item.item==Items.ENCHANTED_BOOK){
+                        book=bookCur
+                        lvl=lvlCur
+                    }
+                    else{
+                        book=""
+                    }
+                }
+                if(slot.index==33){
+                    if(book.isEmpty()){
+                        blocked=false
+                        guiGraphics.renderFakeItem(ItemStack(Items.ENCHANTED_BOOK),80, 54)
+                        return@on
+                    }
+                    if(slot.item.item!=Items.ENCHANTED_BOOK){
+                        blocked=false
+                        guiGraphics.renderFakeItem(ItemStack(Items.WRITABLE_BOOK),80, 54)
+                        return@on
+                    }
+                    if(lvl==lvlCur&&book==bookCur){
+                        blocked=false
+                        guiGraphics.renderFakeItem(ItemStack(Items.END_PORTAL_FRAME),80, 54)
+                        return@on
+                    }
+                    guiGraphics.renderFakeItem(ItemStack(Items.BARRIER),80, 54)
+                    //alert("INVALID COMBINE")
+                    blocked=true
+                }
             }
-            if(switch==2){
-                if(highlightCommission&&lore.contains("Click to claim rewards!")){
+            on<GuiEvent.SlotClick>{
+                if(blocked&&blockWrongAnvil&&slotId==31)cancel()
+            }
+        }
+    }
+    
+    private object CommHelper{
+        init {
+            on<GuiEvent.RenderSlot>{
+                if(slot.item.loreString.contains("Click to claim rewards!")){
                     guiGraphics.fill(slot.x, slot.y, slot.x + 16, slot.y + 16, commissionColor.rgba)
-                    guiGraphics.renderFakeItem(slot.item,slot.x, slot.y)
                 }
-                return@on
             }
-            val customData=slot.item.customData
-            if(!highlightAnvil||!customData.contains("enchantments")) return@on
-            val enchantmentTag=customData.get("enchantments")?.asCompound()?.get()
-            val enchantmentFirst=enchantmentTag?.entrySet()?.first()
-            val enchantmentName= enchantmentFirst?.key.toString()
-            val enchantmentLvl=enchantmentFirst?.value.toString().toInt()
-            if(curBook==enchantmentName&&curLvl==enchantmentLvl){
-                guiGraphics.fill(slot.x, slot.y, slot.x + 16, slot.y + 16, anvilColor.rgba)
-                guiGraphics.renderFakeItem(slot.item,slot.x, slot.y)
-            }
-            if(slot.index==29){
-                if(slot.item.item != Items.ENCHANTED_BOOK){
-                    curLvl=-1
-                    return@on
+        }
+    }
+    
+    private object PetHelper{
+        init {
+            on<GuiEvent.RenderSlot>{
+                if(slot.item.loreString.contains("Click to despawn!")){
+                    guiGraphics.fill(slot.x, slot.y, slot.x + 16, slot.y + 16, petColor.rgba)
                 }
-                curBook=enchantmentName
-                curLvl=enchantmentLvl
-            }
-            if(slot.index==33&&!(curBook==enchantmentName&&curLvl==enchantmentLvl)&&curLvl!=-1){
-                guiGraphics.renderFakeItem(ItemStack(Items.BARRIER),80, 54)
-                alert("INVALID COMBINE")
             }
         }
     }
