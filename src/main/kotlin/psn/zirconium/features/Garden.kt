d11bfaae9787ea70fe7d4ce2e569d9d662719e37
poisonous888 <rwlittle888@gmail.com>
2026-09-09 02:59:49 -0400

    0.0.30 - improved garden

--- src/main/kotlin/psn/zirconium/features/Garden.kt
+++ src/main/kotlin/psn/zirconium/features/Garden.kt
@@ -1,50 +1,129 @@
 package psn.zirconium.features
 
+import com.odtheking.odin.clickgui.settings.impl.ActionSetting
 import com.odtheking.odin.clickgui.settings.impl.NumberSetting
+import com.odtheking.odin.events.ChatMessageEvent
+import com.odtheking.odin.events.LevelEvent
+import com.odtheking.odin.events.core.EventBus
+import com.odtheking.odin.events.core.on
 import com.odtheking.odin.events.core.onReceive
 import com.odtheking.odin.features.Module
-import com.odtheking.odin.utils.alert
+import com.odtheking.odin.utils.handlers.schedule
 import com.odtheking.odin.utils.modMessage
+import com.odtheking.odin.utils.playSoundAtPlayer
 import com.odtheking.odin.utils.render.textDim
+import com.odtheking.odin.utils.setTitle
 import com.odtheking.odin.utils.skyblock.Island
 import com.odtheking.odin.utils.skyblock.LocationUtils
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket
+import net.minecraft.sounds.SoundEvents
 import psn.zirconium.ZirconiumEntry
+import psn.zirconium.zcon
 
 object Garden : Module(
     name = "Garden",
     description = "various garden stuff",
     category=ZirconiumEntry.ZCON
 ) {
-    private val notifyTime by NumberSetting("Notify On Time", 150, 0, 500, desc = "")
+    private val minTimeChange by NumberSetting("Min Time Change",30,0,120,1,"")
     private val timerRegex = Regex("(Cooldown: )([0-9]+m)? ?([0-9]+s)?")
-    private val pestHud by HUD("Pest HUD", desc = "Displays ") {
+    private val pestRegex = Regex("^YUCK! [0-9] \uE07F Pest have spawned in Plot - [0-9]+!$")
+    private val pestHud by HUD("Pest HUD","") {
         editing -> when{
-            LocationUtils.isCurrentArea(Island.Garden)||editing -> textDim(curString, 0, 0)
+        LocationUtils.isCurrentArea(Island.Garden)||editing->textDim(curString,0,0)
             else -> 0 to 0
         }
     }
+    private val test by ActionSetting("Test Alert",""){
+        announce()
+    }
+    fun announce(){
+        setTitle("SWAP")
+        modMessage("Swap Equipment",zcon)
+        for(a in 1..5) {
+            schedule(a*4){
+                playSoundAtPlayer(SoundEvents.ANVIL_LAND,2f,1.5f-0.2f*a)
+            }
+        }
+    }
     var curString=""
-    var primed=false
-    init{
-        onReceive<ClientboundPlayerInfoUpdatePacket> {
-            if(!LocationUtils.isCurrentArea(Island.Garden)){return@onReceive}
-            val tabListEntries=entries().mapNotNull { it.displayName?.string }.ifEmpty { return@onReceive }
-            for (entry in tabListEntries){
-                timerRegex.find(entry)?.destructured?.let { (_,mins,secs) ->
-                    val curMins = mins.substringBefore("m").toIntOrNull() ?: 0
-                    val curSecs = secs.substringBefore("s").toIntOrNull() ?: 0
-                    val curTimer = curMins*60+curSecs
-                    //modMessage("$curTimer - $primed")
-                    curString=entry
-                    if(curTimer<notifyTime&&primed){
-                        modMessage("Pests Off Cooldown!")
-                        alert("Pests Off Cooldown!")
-                        primed=false
+    var curTime=0
+    var timeDiff=0
+    var spawned=false
+    var announced=true
+    fun processTabList(tabListEntries:List<String>){
+        for(entry in tabListEntries) {
+            timerRegex.find(entry)?.destructured?.let {(_,mins,secs) ->
+                val newMins=mins.substringBefore("m").toIntOrNull()?:0
+                val newSecs=secs.substringBefore("s").toIntOrNull()?:0
+                val newTime=newMins*60+newSecs
+                
+                if(spawned){
+                    if(newTime>0){
+                        curTime=newTime
+                        spawned=false
                     }
-                    if(curTimer>notifyTime&&!primed){
-                        primed=true
+                }
+                else{
+                    if(newTime>curTime+minTimeChange){
+                        timeDiff=newTime-curTime
+                        curTime=newTime
                     }
+                    else{
+                        curTime=newTime
+                    }
+                }
+                val adjustedTime=curTime-timeDiff
+                
+                if(adjustedTime>0){
+                    val curSec=adjustedTime%60
+                    val curMin=adjustedTime/60
+                    curString="${curMin}m ${curSec}s Until Spawn"
+                    if(announced)announced=false
+                }
+                else{
+                    if(!announced) {
+                        announced=true
+                        announce()
+                    }
+                    if(curTime>0){
+                        val curSec=curTime%60
+                        val curMin=curTime/60
+                        curString="READY (${curMin}m ${curSec}s)"
+                    }
+                    else{
+                        curString="READY"
+                    }
+                }
+                
+            }
+        }
+    }
+    private object GardenHelper {
+        init {
+            onReceive<ClientboundPlayerInfoUpdatePacket> {
+                val tabListEntries=entries().mapNotNull{it.displayName?.string}.ifEmpty {return@onReceive}
+                processTabList(tabListEntries)
+            }
+            on<ChatMessageEvent>{
+                if(pestRegex.containsMatchIn(value)){
+                    spawned=true
+                    modMessage("spawned!")
+                }
+            }
+//            onReceive<ClientboundAddEntityPacket> {
+//                modMessage("x:$x y:$y z:$z - $type")
+//            }
+        }
+    }
+    init {
+        on<LevelEvent.Load> {
+            EventBus.unsubscribe(GardenHelper)
+            schedule(100,true){
+                if(LocationUtils.isCurrentArea(Island.Garden)) {
+                    EventBus.subscribe(GardenHelper)
+                    modMessage("loaded garden module",zcon)
+                    return@schedule
                 }
             }
         }
