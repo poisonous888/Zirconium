7dedf0b92d7ac371961542487bfcba69bbba5a12
poisonous888 <rwlittle888@gmail.com>
2026-09-17 21:51:40 -0400

    :crying:

--- src/main/kotlin/psn/zirconium/features/CryptAlert.kt
+++ src/main/kotlin/psn/zirconium/features/CryptAlert.kt
@@ -1,6 +1,6 @@
 package psn.zirconium.features
 
-import com.odtheking.odin.events.LocationChangeEvent
+import com.odtheking.odin.events.RoomEnterEvent
 import com.odtheking.odin.events.core.on
 import com.odtheking.odin.features.Module
 import com.odtheking.odin.features.impl.dungeon.map.DungeonScan
@@ -13,13 +13,15 @@ object CryptAlert : Module(
     description = "warns you when there are 75+ crypts in the current dungeon",
     category=ZirconiumEntry.ZCON
 ) {
-    private val hud by HUD("Hud",""){
-        example->if(!example||!DungeonUtils.inDungeons)return@HUD 0 to 0
+    private val hud by HUD("Hud",""){example->
+        if(!example&&!DungeonUtils.inClear){
+            return@HUD 0 to 0
+        }
         textDim("Crypts: ${DungeonUtils.cryptCount}/$total",0,0)
     }
     var total=0
     init {
-        on<LocationChangeEvent>{
+        on<RoomEnterEvent>{
             total=0
             for(room in DungeonScan.rooms){
                 total+=room.data?.crypts?:0
